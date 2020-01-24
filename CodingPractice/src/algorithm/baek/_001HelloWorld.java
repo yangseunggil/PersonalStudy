a017b773820ac636955d22bcec6938dfecb8f508
양승길 <bewitchsk@gmail.com>
2020-01-24 20:48:06 +0900

    Push

--- CodingPractice/src/algorithm/baek/_001HelloWorld.java
+++ CodingPractice/src/algorithm/baek/_001HelloWorld.java
@@ -3,5 +3,7 @@ package algorithm.baek;
 public class _001HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!"); 
+		System.out.println("Hello World!"); 
+		System.out.println("Hello World!"); 
 	}
 }
