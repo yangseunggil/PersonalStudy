eb3ac2b0a6de07f45573dbfe2aee0b0708254a89
YangSeungGIl <bewitchsk@gmail.com>
2021-08-12 15:13:02 +0900

    BinarySearch

--- CodingPractice/src/algorithm/fastcampus/Chapter18SequentialSearch.java
+++ CodingPractice/src/algorithm/fastcampus/Chapter18SequentialSearch.java
@@ -2,16 +2,34 @@ package algorithm.fastcampus;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Chapter18SequentialSearch {
 
-    public int search(ArrayList<Integer> list, int data){
-        for(Integer current : list){
-            if(data == current){
-                return list.indexOf(current);
-            }
+    public boolean search(ArrayList<Integer> list, int data){
+
+        if(list.size() == 1 && list.get(0) == data){
+            return true;
         }
-        return -1;
+        if(list.size() == 1 && list.get(0) != data){
+            return false;
+        }
+        if(list.size() == 0){
+            return false;
+        }
+
+        int mid = list.size()/2;
+        if(list.get(mid) == data){
+            return true;
+        }else if( list.get(mid) > data ){
+            // left
+            return this.search( new ArrayList<Integer>(list.subList(0,mid)), data );
+        }else if ( list.get(mid) < data ){
+            // right
+            return this.search( new ArrayList<Integer>(list.subList(mid, list.size())), data );
+        }else{
+            return false;
+        }
     }
 
 
@@ -22,6 +40,7 @@ public class Chapter18SequentialSearch {
         for (int index = 0; index < 100; index++) {
             testData.add((int)(Math.random() * 100));
         }
+        Collections.sort(testData);
         Chapter18SequentialSearch search = new Chapter18SequentialSearch();
         System.out.println(testData);
         System.out.println(search.search(testData, 4));
